d7881b02f531c7495babb2ac403c3145254c3073
VickyShinny <victoriaoluwaferanmi@gmail.com>
2023-04-12 21:18:35 +0100

    first commit

---
+++ src/components/Counter.vue
@@ -0,0 +1,20 @@
+{/*export default { //data() { return { count: 0, }; }, methods: { increment() {
+this.count++; }, decrement() { this.count--; }, }, }; */}
+<script setup>
+import { ref } from "vue";
+const count = ref(5);
+const increment = () => {
+  count.value++;
+};
+const decrement = () => {
+  count.value--;
+};
+</script>
+
+<template>
+  <div>
+    <h2>count is {{ count }}</h2>
+    <button @click="increment">Inc</button>
+    <button @click="decrement">Dec</button>
+  </div>
+</template>
